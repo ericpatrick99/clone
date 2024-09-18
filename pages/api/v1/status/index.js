8f836bcf9ad030b9518cee861e8a1c2033b71749
Eric Patrick <ericpatrick99@gmail.com>
2024-09-18 01:31:33 +0000

    Add database e compose

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response){
+import database from "../../../../infra/database.js"
+async function status(request, response){
+    const r = await database.query("select 1 + 1 as sum;")
+    console.log(r.rows)
     response.status(200).json({"chave":"valor"});
 }
 export default status;
