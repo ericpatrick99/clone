52dd63b37165c3796e0a610640d1ea4388c25e9a
Eric Patrick <ericpatrick99@gmail.com>
2024-09-19 01:42:28 +0000

    finalizando endpoint /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,24 @@
 import database from "infra/database.js"
 async function status(request, response){
-    const r = await database.query("select 1 + 1 as sum;")
-    console.log(r.rows)
-    response.status(200).json({"chave":"valor"});
+    const valueMaxConnection = await database.query("show max_connections;");
+    const valueVersion = await database.query("show server_version;");
+    const nameDb = process.env.POSTGRES_DB;
+
+    const valueConnectionsOpen = await database.query({
+        text: `select count(*)::int from pg_stat_activity where datname = $1;`,
+        values: [nameDb]
+    });
+
+    const updatedAt = new Date().toISOString();
+    response.status(200).json({
+        updated_at: updatedAt,
+        dependencies:{
+            database:{
+                version: valueVersion.rows[0].server_version,
+                max_connections: parseInt(valueMaxConnection.rows[0].max_connections),
+                opened_connections: valueConnectionsOpen.rows[0].count
+            }
+        }
+    });
 }
 export default status;
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("get para api/v1/status returna 200", async () => {
-    r = await fetch("http://127.0.0.1:3000/api/v1/status")
+    const r = await fetch("http://127.0.0.1:3000/api/v1/status")
     expect(r.status).toBe(200)
-})
+    const responseBody = await r.json()
+    
+    const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+    expect(responseBody.updated_at).toEqual(parseUpdatedAt)
+
+    expect(responseBody.dependencies.database.version).toBeDefined();
+    expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+    expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+    expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+});
